179b656b764590c1f8458ab51261340ddfbca0a9
Birendra Kumar Shaw <krs.birendra@gmail.com>
2018-08-21 15:41:39 +0530

    changing

--- KafkaWorks/src/com/kafka/day2/ClientConsumer.java
+++
@@ -1,8 +0,0 @@
-package com.kafka.day2;
-
-public class ClientConsumer {
-	public static void main(String[] args) {
-		Consumer consumer = new Consumer(KafkaProperties.TOPIC_1, false);
-		consumer.start();
-	}
-}
